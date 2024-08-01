f5ba82f14688dc5828a15e9d77f0f1d6dd52c44a
zaid kamil <xaidmetamorphos@gmail.com>
2024-08-01 15:51:46 +0530

    added some UX

--- app/src/main/java/com/digi/composeforms/MainActivity.kt
+++ app/src/main/java/com/digi/composeforms/MainActivity.kt
@@ -5,6 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,15 +19,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,18 +43,26 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.digi.composeforms.ui.theme.ComposeFormsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,14 +71,217 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFormsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SignUpScreen(
-                        modifier = Modifier.padding(innerPadding)
+                    PagerScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PagerScreen(modifier: Modifier = Modifier) {
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { 3 })
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.one),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            when (it) {
+                0 -> AboutScreen(modifier)
+                1 -> LogInScreen(modifier)
+                2 -> SignUpScreen(modifier)
+            }
+        }
+    }
+}
+
+
+@Composable
+fun LogInScreen(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isFormVisible by remember { mutableStateOf(false) }
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.Transparent),
+        contentAlignment = Alignment.Center
+    ) {
+        AnimatedVisibility(visible = !isFormVisible, enter = fadeIn(), exit = fadeOut()) {
+            ExtendedFloatingActionButton(onClick = {
+                isFormVisible = true
+            }) {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null,
+                )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(text = "Sign In")
+            }
+        }
+        // login form ui
+        AnimatedVisibility(
+            visible = isFormVisible,
+            // slide from right
+            enter = slideInVertically(initialOffsetY = { it }) + fadeIn(),
+            exit = slideOutVertically { it } + fadeOut()
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(32.dp),
+                shape = MaterialTheme.shapes.extraLarge,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp)
+                        .padding(top = 32.dp, bottom = 8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Email,
+                                contentDescription = null
+                            )
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Email
+                        )
+                    )
+                    TextField(
+                        value = password,
+                        onValueChange = {
+                            password = it
+                        },
+                        label = { Text("Password") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Lock,
+                                contentDescription = null
+                            )
+                        },
+                        visualTransformation = PasswordVisualTransformation()
+                    )
+                    ElevatedButton(onClick = {}) {
+                        Text(text = "Log In")
+                    }
+                }
+                IconButton(
+                    onClick = {
+                        // hide the form
+                        isFormVisible = false
+                    },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = null
                     )
                 }
             }
         }
     }
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutScreen(modifier: Modifier = Modifier) {
+    val bottomSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true,
+    )
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    val scope = rememberCoroutineScope()
+    val aboutTextSmall =
+        "😎 A very cool app, it shows a implementation of a login and sign up screen using Jetpack Compose. \n\n📟 With a pager to switch between the screens. The app is built using Jetpack Compose, the modern toolkit for building native UI. It is a declarative API that simplifies UI development. \n\n📱 It is fully compatible with the existing Android UI toolkit and is designed to work with the existing Android development environment."
+    val aboutTextLarge =
+        "This app is built by Zaid Kamil, a passionate Android developer. \n\nHe loves to build apps using the latest technologies. He is a Google Developer Expert for Android and a Kotlin enthusiast. \n\nHe is also a speaker and a writer. He loves to share his knowledge with the community. He is a mentor and a teacher. He is a great person and a great developer. He is a great friend and a great human being. He is a great person and a great developer. He is a great friend and a great human being."
+
+
+    BottomSheetScaffold(
+        sheetContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text(text = "About Author", style = MaterialTheme.typography.headlineSmall)
+
+                Text(
+                    text = aboutTextLarge,
+                    style = MaterialTheme.typography.headlineLarge,
+                    textAlign = TextAlign.Justify,
+                )
+            }
+        },
+        sheetPeekHeight = 0.dp,
+        sheetShape = MaterialTheme.shapes.extraSmall,
+        scaffoldState = scaffoldState,
+        sheetContainerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+        sheetContentColor = MaterialTheme.colorScheme.onSecondaryContainer
+
+    ) {
+
+        Box(
+            modifier = modifier
+                .padding(it)
+                .fillMaxSize()
+                .background(color = Color.Transparent),
+            contentAlignment = Alignment.Center
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(32.dp),
+                shape = MaterialTheme.shapes.extraLarge,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp)
+                        .padding(top = 16.dp, bottom = 16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(text = "About Screen", style = MaterialTheme.typography.headlineSmall)
+                    Text(
+                        text = aboutTextSmall,
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Justify
+                    )
+                    Button(onClick = {
+                        scope.launch {
+                            scaffoldState.bottomSheetState.expand()
+                        }
+                    }) {
+                        Text(text = "Learn More")
+                    }
+                }
+            }
+        }
+        // BottomSheet
+
+    }
+}
+
 
 @Composable
 fun SignUpScreen(modifier: Modifier = Modifier) {
@@ -69,30 +294,37 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.secondary),
+            .background(color = Color.Transparent),
         contentAlignment = Alignment.Center
     ) {
-        AnimatedVisibility(visible = !isFormVisible) {
+        AnimatedVisibility(visible = !isFormVisible, enter = fadeIn(), exit = fadeOut()) {
             ExtendedFloatingActionButton(onClick = {
                 isFormVisible = true
             }) {
                 Icon(
-                    imageVector = Icons.Default.Add,
+                    imageVector = Icons.Default.AddCircle,
                     contentDescription = null,
                 )
-                Spacer(modifier = Modifier.padding(8.dp))
+                Spacer(modifier = Modifier.padding(4.dp))
                 Text(text = "Create new account")
             }
         }
-        AnimatedVisibility(visible = isFormVisible) {
+        AnimatedVisibility(
+            visible = isFormVisible,
+            // slide from right
+            enter = slideInVertically(initialOffsetY = { it }) + fadeIn(),
+            exit = slideOutVertically { it } + fadeOut()
+        ) {
             Card(
                 modifier = Modifier
-                    .padding(16.dp)
+                    .padding(32.dp),
+                shape = MaterialTheme.shapes.extraLarge,
             ) {
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(32.dp),
+                        .padding(horizontal = 32.dp)
+                        .padding(top = 32.dp, bottom = 8.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
@@ -143,15 +375,21 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
                         visualTransformation = PasswordVisualTransformation()
                     )
 
-                    ElevatedButton(onClick = { /*TODO*/ }) {
+                    ElevatedButton(onClick = {
+
+                    }) {
                         Text(text = "Sign Up")
                     }
-                    IconButton(onClick = { isFormVisible = false }) {
-                        Icon(
-                            imageVector = Icons.Default.Close,
-                            contentDescription = null
-                        )
-                    }
+                }
+                IconButton(
+                    onClick = { isFormVisible = false },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = null
+                    )
                 }
             }
         }
@@ -164,3 +402,16 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
 private fun SignUpPreview() {
     SignUpScreen()
 }
+
+@Preview
+@Composable
+private fun LogInPreview() {
+    LogInScreen()
+}
+
+@Preview
+@Composable
+private fun AboutPreview() {
+    AboutScreen()
+}
+
